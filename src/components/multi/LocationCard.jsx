98af31232a44d1b786eb1af72c44bc780cfb2f5d
professional-pigeon <kyle.kayperez@gmail.com>
2023-02-24 12:41:50 -0800

    add temporary layout for location card

--- src/components/multi/LocationCard.jsx
+++ src/components/multi/LocationCard.jsx
@@ -1,11 +1,19 @@
-import { Box, Text } from '@chakra-ui/react'
+import { Flex, Button, Text, VStack, HStack } from '@chakra-ui/react'
 
 export default function LocationCard({ id, name, notes, description }) {
   return (
-    <Box bg='green.500' w='33%' h='150px'>
+    <Flex direction='column' bg='green.500' w='33%' h='fit' p='.25rem' gap='1rem'>
       <Text>{name}</Text>
-      <Text>{description}</Text>
-      <Text>{notes}</Text>
-    </Box>
+      <VStack>
+        <Text w='full'>Description</Text>
+        <Text>{description || 'nada'}</Text>
+      </VStack>
+      <VStack>
+        <Text w='full'>Notes</Text>
+        <Text>{notes || 'nada'}</Text>
+      </VStack>
+      <Button>Go to</Button>
+    </Flex>
   )
 }
+
