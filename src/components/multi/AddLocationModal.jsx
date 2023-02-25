dbf6799a3583852f0c5e7c87dc0195fa0dd32fb6
professional-pigeon <kyle.kayperez@gmail.com>
2023-02-24 16:11:30 -0800

    add modal overlay, modal opens

--- src/components/multi/AddLocationModal.jsx
+++ src/components/multi/AddLocationModal.jsx
@@ -1,7 +1,39 @@
-import { Flex, Button, Text, VStack } from '@chakra-ui/react'
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Text,
+  useDisclosure,
+  Button,
+} from '@chakra-ui/react'
 
 export default function AddLocationModal({ locations, setLocations }) {
+  const { isOpen, onOpen, onClose } = useDisclosure()
   return (
-    <Text>modal holder</Text>
+    <>
+      <Button onClick={onOpen}>Add Location</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Add Location</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>form to go here</Text>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant='ghost'>Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   )
 }
