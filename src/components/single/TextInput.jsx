9cb080ade06d6b6a87d11cfe05cbef8c0b4b039a
professional-pigeon <kyle.kayperez@gmail.com>
2023-03-13 13:19:06 -0700

    add helperText area on input field. Will not display empty helper text

--- src/components/single/TextInput.jsx
+++ src/components/single/TextInput.jsx
@@ -1,17 +1,25 @@
-import { Text, Input, Flex } from '@chakra-ui/react'
+import { FormLabel, Input, Flex, FormHelperText, FormControl } from '@chakra-ui/react'
 import PropTypes from 'prop-types'
 
 export default function TextInput({ 
   name, 
   inputValue, 
   setInputValue, 
-  disabled
+  disabled,
+  helperText,
+  error
 }) {
   return (
-    <Flex w='100%' direction='column'>
-      <Text>{name}</Text>
-      <Input value={inputValue} isDisabled={disabled} onChange={(e) => setInputValue(e.target.value)}/>
-    </Flex>
+    <FormControl>
+      <FormLabel>{name}</FormLabel>
+      <Input 
+        isInvalid={error}
+        isDisabled={disabled} 
+        value={inputValue} 
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      {helperText && <FormHelperText>{helperText}</FormHelperText>}
+    </FormControl>
   )
 }
 
@@ -19,12 +27,16 @@ TextInput.propTypes = {
   name: PropTypes.string,
   inputValue: PropTypes.string,
   setInputValue: PropTypes.string,
-  disabled: PropTypes.bool
+  disabled: PropTypes.bool,
+  error: PropTypes.bool,
+  helperText: PropTypes.string
 }
 
 TextInput.defaultProps ={
   name: '',
   inputValue: '',
   setInputValue: () => console.log('hook not set'),
-  disabled: false
+  disabled: false,
+  error: false,
+  helperText: '',
 }
