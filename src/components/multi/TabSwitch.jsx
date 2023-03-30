972a7ef19de6d3ad6d3fcd749910f50fa452ebb6
professional-pigeon <kyle.kayperez@gmail.com>
2023-03-30 16:30:36 -0700

    create tabSwitch component to switch between what is displayed

---
+++ src/components/multi/TabSwitch.jsx
@@ -0,0 +1,32 @@
+import { Box, Tabs, TabPanel, TabPanels, TabList, Tab} from '@chakra-ui/react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import CharacterCardHolder from './character/CharacterCardHolder';
+import LocationHolder from './location/LocationHolder';
+
+export default function TabSwitch({ locations, characters, campaignId }) {
+  const [tabIndex, setTabIndex] = useState(0)
+
+  const handleTabsChange = (index) => {
+    setTabIndex(index)
+  }
+
+  return (
+    <Box>
+      <Tabs index={tabIndex} onChange={handleTabsChange}>
+        <TabList>
+          <Tab>Locations</Tab>
+          <Tab>Characters</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            {locations?.length > 1 && <LocationHolder locations={locations} campaignId={campaignId} />}
+          </TabPanel>
+          <TabPanel>
+            {characters?.length > 1 && <CharacterCardHolder characters={characters} campaignId={campaignId} />}
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </Box>
+  )
+};
