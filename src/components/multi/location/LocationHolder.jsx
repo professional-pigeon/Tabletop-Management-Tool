de3f1db924e4b7548f13f5c89c80eebdc70877ca
professional-pigeon <kyle.kayperez@gmail.com>
2023-03-30 14:45:45 -0700

    sketch out locationHolder component to control card display

---
+++ src/components/multi/location/LocationHolder.jsx
@@ -0,0 +1,19 @@
+import { Flex } from '@chakra-ui/react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import LocationCard from './LocationCard';
+import { locationShape } from '../../../lib/propShapes';
+
+export default function LocationHolder({ locations }) {
+  return (
+    <Flex direction='row' gap='1rem' flexWrap='wrap' p='1rem'>
+    {locations.length > 0 && locations.map((location) => 
+      <LocationCard key={`${location.id} ${location.name}`} location={location} />
+      )}
+    </Flex>
+  );
+};
+
+LocationHolder.propTypes = {
+  locations: PropTypes.arrayOf(locationShape).isRequired
+};
