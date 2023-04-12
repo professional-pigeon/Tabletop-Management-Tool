ae9002f07f55a03b3f2e9a8c6f0d1d2488a378a2
professional-pigeon <kyle.kayperez@gmail.com>
2023-04-11 18:30:42 -0700

    delete current cookie when going to log in page

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,5 +1,6 @@
 import { Box, Center } from '@chakra-ui/react'
 import React from 'react'
+import Cookies from 'cookies'
 import LoginForm from '../../components/multi/LoginForm'
 
 export default function Index() {
@@ -10,4 +11,13 @@ export default function Index() {
       </Center>
     </Box>
   )
+}
+
+export async function getServerSideProps({req , res}) {
+  const cookies = new Cookies(req, res)
+	cookies.set('_api_tabletop_management_session')
+
+  return {
+    props: {},
+  }
 }
