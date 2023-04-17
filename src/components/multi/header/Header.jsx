1778db6b7f554e93ab30d6838872bb849eaee1b8
professional-pigeon <kyle.kayperez@gmail.com>
2023-04-17 10:51:50 -0700

    header will flip between logout and login link

--- src/components/multi/header/Header.jsx
+++ src/components/multi/header/Header.jsx
@@ -3,6 +3,7 @@ import { Box, Flex, Text } from '@chakra-ui/react';
 import React from 'react';
 import { useRouter } from 'next/router';
 import { logoutCall } from '../../../lib/login';
+import Link from 'next/link';
 
 export default function Header({ user }) {
   const { userName = '' } = user || {};
@@ -11,13 +12,21 @@ export default function Header({ user }) {
   const logoutWrap = () => {
     logoutCall().then(() => router.push('/login'))
   };
+
+  const loginWrap = () => {
+    router.push('/login')
+  }
   
   return (
     <Box bg='blue.300' px='4rem' py='1rem' mb='2rem' boxShadow='lg' borderBottomRadius='sm'>
       <Flex direction='row' fontSize='xl'>
         <Text w='33%'>Campaign Manager</Text>
         <Text w='33%' textAlign='center' >Welcome {userName}</Text>
-        <Text  w='33%' as='button' onClick={() => logoutWrap()}>Logout</Text>
+        {user?.userName ? 
+            <Text  w='33%' as='button' onClick={() => logoutWrap()}>Logout</Text>
+          :
+            <Text  w='33%' as='button' onClick={() => loginWrap()}>Login</Text>
+        }
       </Flex>
     </Box>
   );
