fef046e71cc0d1444cd15a44e70e5882a61ee3bb
professional-pigeon <kyle.kayperez@gmail.com>
2023-04-20 16:11:12 -0700

    add variants for links and for add button colors

--- src/theme/components/Button.js
+++ src/theme/components/Button.js
@@ -1,3 +1,10 @@
+const deleteColor = 'gray.900'
+const deleteHoverColor = 'gray.700'
+const linkColor = 'white'
+const linkHoverColor = 'gray.100'
+const addColor = 'purple.200'
+const addHoverColor = 'purple.100'
+
 const Button = {
   defaultProps: {},
   baseStyle: {},
@@ -6,21 +13,64 @@ const Button = {
   // styles for different visual variants ("outline", "solid")
   variants: {
     'card-delete': {
-      bg: 'red.500',
+      bg: deleteColor,
       color: 'white',
       h: "8",
       minW: "8",
       fontSize: "sm",
       px: "3",
+      _hover: {
+        bg: deleteHoverColor
+      }
     },
     'delete': {
-      bg: 'red.500',
+      bg: deleteColor,
       color: 'white',
       h: "10",
       minW: "10",
       fontSize: "md",
       px: "4",
-    }
+      _hover: {
+        bg: deleteHoverColor
+      }
+    },
+    'card-link': {
+      bg: linkColor,
+      border: '1px solid',
+      borderColor: 'black',
+      color: 'black',
+      h: "8",
+      minW: "8",
+      fontSize: "sm",
+      px: "3",
+      _hover: {
+        bg: linkHoverColor,
+      }
+    },
+    'link': {
+      bg: linkColor,
+      border: '1px solid',
+      borderColor: 'black',
+      color: 'black',
+      h: "10",
+      minW: "10",
+      fontSize: "md",
+      px: "4",
+      _hover: {
+        bg: linkHoverColor,
+      }
+    },
+    'modal-add': {
+      bg: addColor,
+      color: 'black',
+      h: "10",
+      minW: "10",
+      fontSize: "md",
+      px: "4",
+      _hover: {
+        bg: addHoverColor,
+      }
+    },
   },
 } 
 
