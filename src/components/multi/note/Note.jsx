1f6f0029201777ac00b3a7af21f2a5d2caed8abe
professional-pigeon <kyle.kayperez@gmail.com>
2023-04-21 13:11:33 -0700

    note component style and content positioned

--- src/components/multi/note/Note.jsx
+++ src/components/multi/note/Note.jsx
@@ -1,13 +1,32 @@
-import { Box, Text } from '@chakra-ui/react';
+import { Flex, Text, VStack, Button } from '@chakra-ui/react';
 import React from 'react';
 import { noteShape } from '../../../lib/propShapes';
 
 export default function Note({ note }) {
-  console.log(note)
   return (
-    <Box>
-      <Text>Note</Text>
-    </Box>
+    <Flex 
+      border='1px solid' 
+      minW='24px' 
+      px='1rem' 
+      py='1rem'
+      borderRadius='md'
+      boxShadow='md'
+      _hover={{
+        bg: 'purple.100',
+        boxShadow: 'lg'
+      }}
+    >
+      <Text pl='1.5rem' textIndent='-1.5rem' fontWeight='bold'>
+        {`${note.updatedAt}: `}
+        <Text as='span' fontWeight='semibold'>
+          {note.content}
+        </Text>
+      </Text>
+      <VStack>
+        <Button variant='delete-card'>hi</Button>
+        <Button variant='update-card'>hi</Button>
+      </VStack>
+    </Flex>
   );
 };
 
