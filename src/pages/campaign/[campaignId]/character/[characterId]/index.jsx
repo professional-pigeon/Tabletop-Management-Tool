717c9cce01948b61aa270dc6362e7eca595d7993
professional-pigeon <kyle.kayperez@gmail.com>
2023-04-20 17:32:47 -0700

    add campaign context to characterindex page

--- src/pages/campaign/[campaignId]/character/[characterId]/index.jsx
+++ src/pages/campaign/[campaignId]/character/[characterId]/index.jsx
@@ -1,30 +1,36 @@
 /* eslint-disable no-unused-vars */
 import { Flex, Heading, Text } from '@chakra-ui/react'
 import React from 'react'
+import { useRouter } from 'next/router'
 import useCharacters from '../../../../../lib/hooks/useCharacters'
 import Layout from '../../../../../components/single/Layout'
 import { authUser } from '../../../../../lib/user'
 import { userShape } from '../../../../../lib/propShapes'
 import FeatureHolder from '../../../../../components/single/FeatureHolder'
 import UpdateCharacterModal from '../../../../../components/multi/character/UpdateCharacterModal'
+import { CampaignIdProvider } from '@/components/context/CampaignIdContext'
 
 export default function Index(props) {
   const { user } = props
+  const router = useRouter()
+  const { campaignId } = router.query
   const { character, setCharacter } = useCharacters()
 
   return (
-    <Layout user={user}>
-      <Flex direction='column' w='100vw' p='1rem'>
-        <Heading>Character: {character.name}</Heading>
-        <FeatureHolder>
-          <UpdateCharacterModal 
-            character={character} 
-            initialPlace={character.characterLocation} 
-            buttonVariant='update'
-          />
-        </FeatureHolder>
-      </Flex>
-    </Layout>
+    <CampaignIdProvider id={parseInt(campaignId, 10)}>
+      <Layout user={user}>
+        <Flex direction='column' w='100vw' p='1rem'>
+          <Heading>Character: {character.name}</Heading>
+          <FeatureHolder>
+            <UpdateCharacterModal 
+              character={character} 
+              initialPlace={character.characterLocation} 
+              buttonVariant='update'
+            />
+          </FeatureHolder>
+        </Flex>
+      </Layout>
+      </CampaignIdProvider>
   )
 }
 
