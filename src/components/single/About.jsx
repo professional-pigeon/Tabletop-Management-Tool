db1c93ce63b5c7acc592180f7f18f5ad08242aaf
professional-pigeon <kyle.kayperez@gmail.com>
2023-04-24 11:06:58 -0700

    update about: include tech stack info

--- src/components/single/About.jsx
+++ src/components/single/About.jsx
@@ -4,11 +4,12 @@ import React from 'react';
 export default function About() {
 
   return (
-    <Flex direction='column' w='26rem' px='1rem' py='.5rem' borderRadius='.5rem' boxShadow='lg' gap={2} bg='white'>
+    <Flex direction='column' w='26rem' px='2rem' py='1rem' borderRadius='.5rem' boxShadow='lg' gap={2} bg='white'>
       <Heading textAlign='center'>About</Heading>
       <VStack>
         <Text textIndent='2rem'>Hi, this is a TTRPG management app. Created for use by myself and a few friends, if you have questions or requests for features please email kyle.kayperez@gmail.com.</Text>
-        <Text textIndent='2rem'>If you are someone who is looking at this profile as part of a hiring team and would like to see a version with a lot of data please click here to log in as a test user</Text>
+        <Text textIndent='2rem'>This front end app uses React, Next.js, Storybook, and Chakra UI; a component library which has strengths in theme application, aria functions, and accessibility features.</Text>
+        <Text textIndent='2rem'>The api is also self-built and hosted on Heroku and was made using RoR. I chose to keep them separate as I was most recently working at a company that was transitioning their RoR front end to Next.js and I like the separation of functionality.</Text>
       </VStack>
     </Flex>
   );
