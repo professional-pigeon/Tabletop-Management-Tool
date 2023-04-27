d0596b6ff1e898c2f02de5fc605aa358f0797cca
professional-pigeon <kyle.kayperez@gmail.com>
2023-04-26 22:15:13 -0700

    Will log you in successfully

--- cypress/e2e/login/index.cy.js
+++ cypress/e2e/login/index.cy.js
@@ -1,4 +1,4 @@
-describe('template spec', () => {
+describe('log in', () => {
   it('should show you log in errors or error toast', () => {
     cy.visit('http://localhost:4000/login')
     cy.get('.chakra-button').contains('Login').click()
@@ -11,8 +11,9 @@ describe('template spec', () => {
 
   it('should log you in with correct data', () => {
     cy.visit('http://localhost:4000/login')
-    cy.get('input[id="input-User Name"]').type('Test_Admin')
-    cy.get('input[id="input-Password"]').type('WaffleH0use')
+    cy.get('input[id="input-User Name"]').type('admin_kk')
+    cy.get('input[id="input-Password"]').type('test123')
     cy.get('.chakra-button').contains('Login').click()
+    cy.contains('Welcome admin_kk')
   })
 })
